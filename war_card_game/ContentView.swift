a488c3695729cd2aee58673a4605dd9595ebd558
devAYZ <fatokimiemman@gmail.com>
2023-02-22 14:09:09 +0100

    add project file

---
+++ war_card_game/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  war_card_game
+//
+//  Created by Ayokunle Pro on 2/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war_card_game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war_card_game
+//
+//  Created by Ayokunle Pro on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
